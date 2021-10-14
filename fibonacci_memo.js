16f60eda66fbc5fb6325394c790c8ca913aac91c
Rohan Kokane <rohankokane19@gmail.com>
2021-10-15 01:10:23 +0530

    Create fibonacci_memo.js

---
+++ fibonacci_memo.js
@@ -0,0 +1,22 @@
+var fibonacci = (function() {
+  var memo = {};
+
+  function f(n) {
+    var value;
+
+    if (n in memo) {
+      value = memo[n];
+    } else {
+      if (n === 0 || n === 1)
+        value = n;
+      else
+        value = f(n - 1) + f(n - 2);
+
+      memo[n] = value;
+    }
+
+    return value;
+  }
+
+  return f;
+})();
