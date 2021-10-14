33ef2a9c9f173f6c23c05d5d0bb534586dc3544f
Rohan Kokane <rohankokane19@gmail.com>
2021-10-15 01:07:55 +0530

    Create memoizer.js

---
+++ memoizer.js
@@ -0,0 +1,14 @@
+function memoizer(fun) {
+  let cache = {}
+
+  return function(n) {
+    if (cache[n] != undefined ) {
+      return cache[n]
+    } else {
+      let result = fun(n)
+      cache[n] = result
+
+      return result
+    }
+  }
+}
